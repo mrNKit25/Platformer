1cc0dc557cbac3a3c5b2c69b88d98f568bbd1584
Naoki Kita <naokikita12@yahoo.com>
2020-04-06 17:40:56 -0700

    add player jump and idle animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,15 +9,37 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 25f;
 
     float horizontalMove = 0f;
+    bool jumpFlag = false;
+    bool jump = false;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (jumpFlag)
+        {
+            jumpFlag = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    public void OnLanding()
+    {
+        jump = false;
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        if (jump)
+        {
+            jumpFlag = true;
+        }
     }
 }
