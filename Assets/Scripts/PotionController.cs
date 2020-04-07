4575b5d3c5c427b2a515a0fcbe0564e3a2e3a446
Naoki Kita <naokikita12@yahoo.com>
2020-04-07 14:21:29 -0700

    adding potion

---
+++ Assets/Scripts/PotionController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionController : MonoBehaviour
+{
+    public enum PotionType
+    {
+        Speed,
+        Jump
+    }
+
+    public PotionType potiontype;
+    public int potionModAmount = 0; //how much the potion will modify whatever the property it is modifying 
+
+    private float floatingTimer = 0f;
+    private float floatingMax = 1f;
+    private float floatingDir = 0.01f;
+
+    private void FixedUpdate()
+    {
+        if (floatingTimer < floatingMax)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + floatingDir);
+            floatingTimer += Time.fixedDeltaTime;
+        }
+        else
+        {
+            floatingDir *= -1;
+            floatingTimer = 0f; 
+        }
+    }
+
+}
