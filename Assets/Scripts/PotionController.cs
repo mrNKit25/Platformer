b995803cc58c70543d4e0bb3f985ca4463600654
Naoki Kita <naokikita12@yahoo.com>
2020-04-09 23:03:56 -0700

    Floating movement script

--- Assets/Scripts/PotionController.cs
+++ Assets/Scripts/PotionController.cs
@@ -13,24 +13,6 @@ public class PotionController : MonoBehaviour
     public PotionType potionType;
     public int potionModAmount = 0; //how much the potion will modify whatever the property it is modifying 
 
-    private float floatingTimer = 0f;
-    private float floatingMax = 1f;
-    private float floatingDir = 0.01f;
-
-    private void FixedUpdate()
-    {
-        if (floatingTimer < floatingMax)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y + floatingDir);
-            floatingTimer += Time.fixedDeltaTime;
-        }
-        else
-        {
-            floatingDir *= -1;
-            floatingTimer = 0f; 
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer == 9)
